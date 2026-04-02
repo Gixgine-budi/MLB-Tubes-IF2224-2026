21b0088e854dfa2ba5128db8180ba27c741075f4
Muhammad Akmal <muhammad.akmal.3806@gmail.com>
2026-04-02 22:47:59 +0700

    fix: add output file writing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "lexer/char_machine.hpp"
 #include "lexer/lexer.hpp"
@@ -19,10 +20,23 @@ int main(int argc, char* argv[]) {
     Lexer lexer(reader);
     lexer.run();
 
+    std::cout << "Lexical analysis completed successfully.\n";
+    std::cout << "Read " << reader.line_num() << " lines. and "
+              << lexer.tokens().size() << " tokens.\n";
+
+    std::cout << "Enter output file path > ";
+    std::getline(std::cin, filepath);
+    std::ofstream output_file(filepath);
+    if (!output_file.is_open())
+      throw std::runtime_error("Failed to open output file: " + filepath);
+
     auto tokens = lexer.tokens();
     for (const auto& token : tokens) {
-      std::cout << token << '\n';
+      output_file << token << '\n';
     }
+
+    std::cout << "Tokens have been written to " << filepath << std::endl;
+
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << std::endl;
     return 1;
