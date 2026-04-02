cd753e179f4de620280fa0deb5dcf28e1d88ef26
Muhammad Akmal <muhammad.akmal.3806@gmail.com>
2026-04-02 21:32:20 +0700

    feat: implement main program and update `CMakeLists.txt`

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
-#include "lexer/char_machine.hpp"
-#include "lexer/dfa_graph.hpp"
 #include <iostream>
 
-int main(){
-    std::cout << "Hello, World!" << std::endl;
+#include "lexer/char_machine.hpp"
+#include "lexer/lexer.hpp"
+
+int main(int argc, char* argv[]) {
+  std::string filepath;
+
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <filepath>" << std::endl;
+    return 1;
+  }
+
+  filepath = argv[1];
+
+  // Lexical Analyzer
+  try {
+    CharMachine reader(filepath);
+    Lexer lexer(reader);
+    lexer.run();
+
+    auto tokens = lexer.tokens();
+    for (const auto& token : tokens) {
+      std::cout << token << '\n';
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
 }
